4423f7e8089cdb16d1678d213705bd24a42442c3
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-10-07 22:07:40 +0500

    Update left rotate array

--- segregateEvenOdd.js
+++ segregateEvenOdd.js
@@ -10,20 +10,19 @@
 
 function segregateEvenOdd(arr) {
     let left = 0; // Pointer for the left side of the array
-    let right = arr.length - 1; // Pointer for the right side of the array
+    let right = arr.length - 1;
   
     while (left < right) {
-      // Move the left pointer to the right until an odd number is encountered
+      
       while (arr[left] % 2 === 0 && left < right) {
         left++;
       }
   
-      // Move the right pointer to the left until an even number is encountered
+      
       while (arr[right] % 2 === 1 && left < right) {
         right--;
       }
-  
-      // Swap the elements at left and right pointers
+        
       if (left < right) {
         const temp = arr[left];
         arr[left] = arr[right];
@@ -37,5 +36,5 @@ function segregateEvenOdd(arr) {
   const arr = [12, 34, 45, 9, 8, 90, 3];
   segregateEvenOdd(arr);
   
-  console.log(arr); // Output: [12, 34, 8, 90, 45, 9, 3]
+  console.log(arr); 
   
